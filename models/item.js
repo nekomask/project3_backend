fba4b76b047fe79147f77a8f1ed660cc30bceee2
kittenmasque <volaju@gmail.com>
2022-03-29 09:25:15 -0700

    added to item bike schema

--- models/item.js
+++ models/item.js
@@ -2,8 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-productName: {type:String, default: "grass", required: true},
-quantity: {type:Number, default: 0, required: true}
+productName: {type:String},
+frameType: {type: String },
+frameHeight: {type: String},
+forkBrand: {type: String},
+forkType: {type: String},
+headsetSize: {type: String},
+headsetType: {type:String},
+stemLength: {type:String},
+stemClampSize: {type:String},
+stemAngle: {type:String},
+handlebarType: {type:String},
+seatPostBrand: {type: String},
+seatPostDiameter: {type: String},
+saddleBrand: {type: String},
+brakeType: {type:String},
+frontBrakeBrand: {type: String},
+rearBrakeBrand: {type: String},
+chainRingTeeth: {type: String},
+chainRingBCD: {type: String},
+bottomBracketSize: {type: String},
+bottomBracketType: {type: String},
+crankArmLength: {type: String},
+rimSize: {type: String},
+tireSize: {type: String},
+forkEndSpacing: {type: String},
+dropoutSpacing: {type: String},
+hubTypeFront: {type: String},
+hubTypeRear: {type: String},
+hubLengthFront: {type: String},
+hubLengthRear: {type: String},
+hubSpokeCountFront: {type: String},
+hubSpokeCountRear: {type: String},
+spokeLengthFront: {type: String},
+spokeLengthRear: {type: String},
 }, { timestamps: true })
 
 const Item = mongoose.model('Item', itemSchema);
